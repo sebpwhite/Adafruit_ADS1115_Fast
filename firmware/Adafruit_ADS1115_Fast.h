8811ef16248bb09654ece3207b07c6b9b00f469e
sebpwhite <seb.p.white@gmail.com>
2016-03-06 11:57:55 +1100

    Removed Comparator feature

--- firmware/Adafruit_ADS1115_Fast.h
+++ firmware/Adafruit_ADS1115_Fast.h
@@ -147,9 +147,9 @@ protected:
   uint16_t  readADC_SingleEnded(uint8_t channel);
   int16_t   readADC_Differential_0_1(void);
   int16_t   readADC_Differential_2_3(void);
-  void      startComparator_SingleEnded(uint8_t channel, int16_t threshold);
   void      setGain(adsGain_t gain);
   void      startSampling_SingleEnded(uint8_t channel, adsSampleRate_t samplerate);
+  void      stopSampling(void);
   int16_t   getADCReading();
   adsGain_t getGain(void);
 
